3a857117cb38160e0000fe1ec2c0afc759cf0b6b
VERTIGOJACK <vertigo.digital.design@gmail.com>
2023-08-29 18:06:34 +0200

    Add project files.

---
+++ Classes/Employee/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeUID;
+
+namespace Advanced_Csharp_Lab1.Classes.Employee
+{
+    public abstract class Employee
+    {
+        //props
+        protected int PayrollUID { get; set; }
+        protected string Name { get; set; }
+        protected decimal BaseSalary { get; set; }
+
+        //methods
+        public abstract string getReport();
+        public abstract decimal getSalary();
+
+        //constructors
+        //using the static class imported from dll assign a unique ID on constructed
+        public Employee()
+        {
+            PayrollUID = EmployeeID.getID();
+        }
+        public Employee(string Name)
+        {
+            PayrollUID = EmployeeID.getID();
+            this.Name = Name;
+        }
+        public Employee(string Name, decimal BaseSalary)
+        {
+            PayrollUID = EmployeeID.getID();
+            this.Name = Name;
+            this.BaseSalary = BaseSalary;
+        }
+    }
+}
---
+++ Classes/Employee/Programmer/Programmer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Advanced_Csharp_Lab1.Classes.Employee.Programmer
+{
+    public class Programmer : Employee
+    {
+        // constructors
+        public Programmer(string Name) : base(Name) 
+        { 
+            
+        }
+
+        public Programmer(string Name, decimal Salary) : base(Name,Salary)
+        {
+
+        }
+
+        public Programmer(string Name, decimal Salary, bool NewHire) : base(Name, Salary)
+        {
+            this.NewHire = NewHire;
+        }
+
+        private int SpecializationID;
+
+        private int AssignedMentorID;
+
+        private bool Inexperienced = true;
+
+        public void ToggleInxperienced()
+        {
+            this.Inexperienced = !this.Inexperienced;
+        }
+
+        public override string getReport()
+        {
+            //declare result variable
+            string report;
+            //first the easy part, name and uid
+            report = "Payroll ID: "+base.PayrollUID+"\n";
+            report += "Name: " + base.Name + "\n";
+
+
+            return report;
+        }
+
+        public override decimal getSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
